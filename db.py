0315b4b7b7a3f3e86957bc1260666f051b9e5df7
megatocha <127956346+megatocha@users.noreply.github.com>
2025-10-14 19:42:01 +0200

    New professions/resources, new tags "crafts"/"collects", "for"/"from"

--- db.py
+++ db.py
@@ -27,19 +27,22 @@ async def init_db():
         await conn.run_sync(Base.metadata.create_all)
         async with SessionLocal() as session:
             resources_to_add = [
-                {"name": "Зерно", "emoji": "🌾", "description": "Основной продукт земледелия", "category": "Еда", "rarity": models.RarityLevel.COMMON}, # 1
-                {"name": "Картофель", "emoji": "🥔", "description": "Корнеплод для питания", "category": "Еда", "rarity": models.RarityLevel.COMMON}, # 2
-                {"name": "Грибы", "emoji": "🍄‍🟫", "description": "Лесные грибы", "category": "Еда", "rarity": models.RarityLevel.UNCOMMON}, # 3
-                {"name": "Ягоды", "emoji": "🫐", "description": "Разнообразные собранные в лесу ягоды", "category": "Еда", "rarity": models.RarityLevel.UNCOMMON}, # 4
-                {"name": "Рыба", "emoji": "🐟", "description": "Пойманная в реке рыба", "category": "Еда", "rarity": models.RarityLevel.COMMON}, # 5
-                {"name": "Шерсть", "emoji": "☁️", "description": "Шерсть овец для создания ткани (🧺)", "category": "Материалы", "rarity": models.RarityLevel.COMMON}, # 6
-                {"name": "Молоко", "emoji": "🥛", "description": "Молоко полученное из коровы", "category": "Еда", "rarity": models.RarityLevel.COMMON}, # 6
-                {"name": "Ткань", "emoji": "🧺", "description": "Ткань из шерсти (☁️) для создания бинтов (🩹) и одежды", "category": "Материалы", "rarity": models.RarityLevel.UNCOMMON}, # 7
-                {"name": "Лён", "emoji": "🪴", "description": '<a href="https://ru.wikipedia.org/wiki/Лён">Лён</a> для создания <a href="https://ru.wikipedia.org/wiki/Льняное_масло">лечебных масел</a> и пропитывания ими ткани (🧺) для создания бинтов (🩹)', "category": "Материалы", "rarity": models.RarityLevel.COMMON}, # 8
-                {"name": "Кора", "emoji": "🎋", "description": 'Кора <a href="https://ru.wikipedia.org/wiki/Осина">осиного дерева</a> для создания лечебных отваров (🍵)', "category": "Материалы", "rarity": models.RarityLevel.UNCOMMON}, # 9
-                {"name": "Отвар", "emoji": "🍵", "description": "Целебный отвар из коры осины (🎋) для лечения болезней", "category": "Лекарства", "rarity": models.RarityLevel.UNCOMMON}, # 10 - 3 отвара для лечения болезни
-                {"name": "Бинт", "emoji": "🩹", "description": "Пропитанная лечебными маслами из льна (🪴) ткань для перевязки ран", "category": "Лекарства", "rarity": models.RarityLevel.RARE}, # 11 - 1 бинт для лечения раны
-                {"name": "Сигара", "emoji": "🚬", "description": "ток для админов", "category": "прочитал лох", "rarity": models.RarityLevel.COMMON} # 12
+                {"name": "Зерно", "emoji": "🌾", "description": "Основной продукт земледелия", "category": "Еда", "rarity": models.RarityLevel.COMMON}, # 
+                {"name": "Картофель", "emoji": "🥔", "description": "Корнеплод для питания", "category": "Еда", "rarity": models.RarityLevel.COMMON}, # 
+                {"name": "Грибы", "emoji": "🍄‍🟫", "description": "Лесные грибы", "category": "Еда", "rarity": models.RarityLevel.UNCOMMON}, # 
+                {"name": "Ягоды", "emoji": "🫐", "description": "Разнообразные собранные в лесу ягоды", "category": "Еда", "rarity": models.RarityLevel.UNCOMMON}, # 
+                {"name": "Рыба", "emoji": "🐟", "description": "Пойманная в реке рыба", "category": "Еда", "rarity": models.RarityLevel.COMMON}, # 
+                {"name": "Шерсть", "emoji": "☁️", "description": "Шерсть овец для создания ткани (🧺)", "category": "Материалы", "for": "🧺", "rarity": models.RarityLevel.COMMON}, # 
+                {"name": "Молоко", "emoji": "🥛", "description": "Полученное из коровы молоко", "category": "Еда", "rarity": models.RarityLevel.COMMON}, # 
+                {"name": "Мясо", "emoji": "🍖", "description": "Полученное из животного мясо", "category": "Еда", "rarity": models.RarityLevel.COMMON}, # 
+                {"name": "Ткань", "emoji": "🧺", "description": "Ткань из шерсти (☁️) для создания бинтов (🩹) и одежды", "category": "Материалы", "from": "☁️", "for": "🩹", "rarity": models.RarityLevel.UNCOMMON}, # 
+                {"name": "Нити", "emoji": "🧵", "description": "Нитки из шерсти (☁️) для пошива одежды", "category": "Материалы", "from": "☁️", "rarity": models.RarityLevel.UNCOMMON}, # 
+                {"name": "Сырьё", "emoji": "🔩", "description": "Сырьё из руды (🪨) для создания мебели", "category": "Материалы", "from": "🪨", "rarity": models.RarityLevel.UNCOMMON}, # 
+                {"name": "Лён", "emoji": "🪴", "description": '<a href="https://ru.wikipedia.org/wiki/Лён">Лён</a> для создания <a href="https://ru.wikipedia.org/wiki/Льняное_масло">лечебных масел</a> и пропитывания ими ткани (🧺) для создания бинтов (🩹)', "category": "Материалы", "for": "🩹", "rarity": models.RarityLevel.COMMON}, # 
+                {"name": "Кора", "emoji": "🎋", "description": 'Кора <a href="https://ru.wikipedia.org/wiki/Осина">осиного дерева</a> для создания лечебных отваров (🍵)', "category": "Материалы", "for": "🍵", "rarity": models.RarityLevel.UNCOMMON}, # 
+                {"name": "Отвар", "emoji": "🍵", "description": "Целебный отвар из коры осины (🎋) для лечения болезней", "category": "Лекарства", "from": "🎋", "rarity": models.RarityLevel.UNCOMMON}, #  - 3 отвара для лечения болезни
+                {"name": "Бинт", "emoji": "🩹", "description": "Пропитанная лечебными маслами из льна (🪴) ткань (🧺) для перевязки ран", "category": "Лекарства", "from": "🪴+🧺", "rarity": models.RarityLevel.RARE}, #  - 1 бинт для лечения раны
+                {"name": "Сигара", "emoji": "🚬", "description": "ток для админов", "category": "прочитал лох", "rarity": models.RarityLevel.COMMON} # 
             ]
             for resource in resources_to_add:
                 resource_data = resource.copy()
@@ -47,9 +50,11 @@ async def init_db():
                 await session.execute(text("INSERT INTO resources (name, emoji, description, category, rarity) VALUES (:name, :emoji, :description, :category, :rarity) ON CONFLICT (name) DO NOTHING"), resource_data)
  
             professions_to_add = [
-                {"name": "Землепашец", "emoji": "🌻", "description": "Жнёт 🌾/🥔/🍄‍🟫/🫐 упорно трудясь в поле али лесу.", "required_level": 0}, # 1
-                {"name": "Ловчий", "emoji": "🐾", "description": "В лесу силки ставит, зверя бьёт да рыбу сетью тянет, скотину пасёт, чтоб 🐟/🍖/🥛/☁️ в общину нести.", "required_level": 0}, # 2
-                {"name": "Знахарь", "emoji": "📔", "description": "Внемлет мольбам селян, варит 🍵/🩹, собирая 🪴/🎋 своими руками.", "required_level": 0} # 3
+                {"name": "Землепашец", "emoji": "🌻", "description": "Жнёт 🌾/🥔/🍄‍🟫/🫐 упорно трудясь в поле али лесу.", "collects": "🌾/🥔/🍄‍🟫/🫐", "required_level": 0}, # 1
+                {"name": "Ловчий", "emoji": "🐾", "description": "В лесу силки ставит, зверя бьёт да рыбу сетью тянет, скотину пасёт, чтоб 🐟/🍖/🥛/☁️ в общину нести.", "collects": "🐟/🍖/🥛/☁️", "required_level": 0}, # 2
+                {"name": "Знахарь", "emoji": "📔", "description": "Внемлет мольбам селян, варит 🍵/🩹, собирая 🪴/🎋 своими руками.", "crafts": "🍵/🩹", "collects": "🪴/🎋", "required_level": 0}, # 3
+                {"name": "Ремесленник", "emoji": "⚒️", "description": "", "crafts": "🧺/🧵/🔩", "required_level": 0}, # 4
+                {"name": "Мастеровой", "emoji": "🧰", "description": "", "crafts": "🥾/🧥/🪑/🗡", "required_level": 0} # 5
             ]
             for profession in professions_to_add:
                 await session.execute(text("INSERT INTO professions (name, emoji, description, required_level) VALUES (:name, :emoji, :description, :required_level) ON CONFLICT (name) DO NOTHING"), profession)
